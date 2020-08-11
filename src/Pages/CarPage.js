ca74c46af39471499ef9c7b2ca126e15dcf63004
Richar <ripupo88@gmail.com>
2020-08-11 20:31:02 +0100

    new page car

---
+++ src/Pages/CarPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { MDBBtn, MDBCol, MDBContainer, MDBRow } from "mdbreact";
+import { RPRLightBox } from "../Components/RPRLightBox/RPRLightBox";
+
+export const CarPage = () => {
+    return (
+        <MDBCol md="8">
+            <RPRLightBox />
+        </MDBCol>
+    );
+};
--- src/Routers/RouterApp.js
+++ src/Routers/RouterApp.js
@@ -2,12 +2,16 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { HomePage } from "../Pages/HomePage";
 import { NosotrosPage } from "../Pages/NosotrosPage";
+import { CarPage } from "../Pages/CarPage";
 
 export const RouterApp = () => {
     return (
         <Switch>
             <Route exact path="/nosotros">
                 <NosotrosPage />
+            </Route>
+            <Route exact path="/car">
+                <CarPage />
             </Route>
             <Route path="/">
                 <HomePage />
